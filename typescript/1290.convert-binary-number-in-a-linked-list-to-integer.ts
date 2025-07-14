ed2d3e776f1a38c11b562208b113fe78c55fb534
chenmijiang <jack.chenyuana@gmail.com>
2025-07-14 14:44:50 +0800

    feat(20250714): daily question

---
+++ typescript/1290.convert-binary-number-in-a-linked-list-to-integer.ts
@@ -0,0 +1,127 @@
+/*
+ * @lc app=leetcode.cn id=1290 lang=typescript
+ * @lcpr version=30204
+ *
+ * [1290] 二进制链表转整数
+ *
+ * https://leetcode.cn/problems/convert-binary-number-in-a-linked-list-to-integer/description/
+ *
+ * algorithms
+ * Easy (80.15%)
+ * Likes:    192
+ * Dislikes: 0
+ * Total Accepted:    109.5K
+ * Total Submissions: 135.7K
+ * Testcase Example:  '[1,0,1]'
+ *
+ * 给你一个单链表的引用结点 head。链表中每个结点的值不是 0 就是 1。已知此链表是一个整数数字的二进制表示形式。
+ *
+ * 请你返回该链表所表示数字的 十进制值 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：head = [1,0,1]
+ * 输出：5
+ * 解释：二进制数 (101) 转化为十进制数 (5)
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：head = [0]
+ * 输出：0
+ *
+ *
+ * 示例 3：
+ *
+ * 输入：head = [1]
+ * 输出：1
+ *
+ *
+ * 示例 4：
+ *
+ * 输入：head = [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
+ * 输出：18880
+ *
+ *
+ * 示例 5：
+ *
+ * 输入：head = [0,0]
+ * 输出：0
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 链表不为空。
+ * 链表的结点总数不超过 30。
+ * 每个结点的值不是 0 就是 1。
+ *
+ *
+ */
+
+// @lcpr-template-start
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function getDecimalValue(head: ListNode | null): number {
+  let result = 0;
+  
+  while (head) {
+    // 左移一位（相当于乘2）并加上当前位的值
+    result = (result << 1) | head.val;
+    head = head.next;
+  }
+  
+  return result;
+}
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [1,0,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0,0]\n
+// @lcpr case=end
+
+ */
